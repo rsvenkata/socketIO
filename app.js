1bb500a402e79912cb1c46bb9728195812d51888
Raghunath.Venkata <sv_raghunath@hotmail.com>
2018-11-24 01:14:51 +0000

    Chat

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const socketIO = require('socket.io')
 const port = process.env.PORT || 3003
 const publicPath = path.join(__dirname, './public')
 console.log(publicPath)
+const moment = require('moment')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -20,7 +21,17 @@ const io = socketIO(server.listen(port, () => {
 
 io.on('connection', (socket) => {
     console.log(`Client with ${socket.id} is connected!`)
+    io.emit('newUserJoin', {sockName : socket.id})
+
     socket.on('disconnect', () => {
         console.log(`Client with ${socket.id} is dis-connected!`)
     })
+
+    socket.on('createMessage', (data, callback) => {
+        io.emit('studInfo', {data: data, sockName: socket.id, time: moment().format('Do MMM YY')})
+        callback()
+    })
 })
+
+// mongodb://<dbuser>:<dbpassword>@ds211504.mlab.com:11504/mlabdb
+// mlab API key - mLKK7m_9Iv5jthJ6NyLdU97bZmb8jsOP
---
+++ public/index.js
@@ -0,0 +1,39 @@
+
+let socket = io()
+socket.on('connect', () => {
+    console.log(` I am connected with the server!`)
+})
+
+jQuery('#send-location').on('click', () => {
+  
+    if(!navigator.geolocation)
+      return alert('Geo not available!')
+
+      navigator.geolocation.getCurrentPosition((position) => {
+          console.log(position)
+      }, () => {
+          console.log('Unable to get location')
+      })
+})
+
+jQuery('#message-form').on('submit', (e) => {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        name: jQuery('[name=message]').val()
+    }, () => {
+        jQuery('[name=message]').val('')
+    })
+})
+
+socket.on('studInfo', (data) => {
+    let li1 = jQuery('<li></li>');
+    li1.text(`User says ==> ${data.data.name} @ ${data.time}`)  //${data.sockName} :
+    jQuery('#Messages').append(li1)
+})
+
+socket.on('newUserJoin', (data) => {
+let li2 = jQuery('<li></li>');
+    //jQuery('#Messages').append(li2.text(`${data.sockName} joined the chat`))
+    jQuery('#Messages').append(li2.text(`New user joined the chat`))
+})
